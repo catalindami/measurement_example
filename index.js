42a7d0738931fc72818826c2407a05f69bb72c7b
cdamian <catalindami@yahoo.com>
2019-07-24 12:13:49 +0300

    test

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-module.exports = $$.library(function(){
+module.exports = $$.library(function()
+{
     require("./assets/Token.js");
     require("./assets/Account.js");
+
+
+
+
+
+
+
 
     require("./transactions/tokenManagement.js");
     require("./transactions/accountManagement.js");
